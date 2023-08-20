ca8b431ccc05db9bed16f4f26bbcce9aa7dd3057
picasso <omiddellcash@gmail.com>
2023-08-20 13:17:26 +0330

    third concepts widget example

--- lib/main.dart
+++ lib/main.dart
@@ -15,80 +15,107 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: const RegistrationPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+class RegistrationPage extends StatefulWidget {
+  const RegistrationPage({super.key});
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _RegistrationPageState createState() => _RegistrationPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  final String _displayText = 'Hello, Widget Concepts!';
-  bool _isBoxVisible = true;
-  double _containerWidth = 100.0;
-  Color _boxColor = Colors.red;
+class _RegistrationPageState extends State<RegistrationPage> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
 
-  void _toggleVisibility() {
-    setState(() {
-      _isBoxVisible = !_isBoxVisible;
-    });
-  }
+  final bool _registrationSuccessful = false;
 
-  void _toggleContainerSize() {
-    setState(() {
-      _containerWidth = _containerWidth == 100.0 ? 200.0 : 100.0;
-    });
-  }
-
-  void _toggleBoxColor() {
-    setState(() {
-      _boxColor = _boxColor == Colors.red ? Colors.blue : Colors.red;
-    });
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      // Perform registration logic (simulated here)
+      // If registration is successful, navigate to the success screen
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const SuccessPage()),
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Widget Concepts Example'),
+        title: const Text('Registration Page'),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                controller: _nameController,
+                decoration: const InputDecoration(labelText: 'Name'),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Name is required';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16),
+              TextFormField(
+                controller: _emailController,
+                decoration: const InputDecoration(labelText: 'Email'),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Email is required';
+                  } else if (!value.contains('@')) {
+                    return 'Invalid email format';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: _submitForm,
+                child: const Text('Register'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SuccessPage extends StatelessWidget {
+  const SuccessPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Registration Success'),
+      ),
+      body: const Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            if (_isBoxVisible)
-              AnimatedContainer(
-                width: _containerWidth,
-                height: 100.0,
-                color: _boxColor,
-                duration: const Duration(seconds: 1),
-                curve: Curves.easeInOut,
-                child: const Center(
-                  child: Text('Container'),
-                ),
-              ),
-            const SizedBox(height: 20),
+            Icon(
+              Icons.check_circle,
+              color: Colors.green,
+              size: 100,
+            ),
+            SizedBox(height: 16),
             Text(
-              _displayText,
-              style: const TextStyle(fontSize: 20),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _toggleVisibility,
-              child: Text(_isBoxVisible ? 'Hide Box' : 'Show Box'),
-            ),
-            ElevatedButton(
-              onPressed: _toggleContainerSize,
-              child: const Text('Toggle Container Size'),
-            ),
-            ElevatedButton(
-              onPressed: _toggleBoxColor,
-              child: const Text('Toggle Box Color'),
+              'Registration successful!',
+              style: TextStyle(fontSize: 20),
             ),
           ],
         ),
